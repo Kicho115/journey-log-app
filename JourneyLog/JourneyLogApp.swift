71dee2d4cb5cc82e457e7bd25654f9b49e1583b9
Kicho115 <oscar.leonardo.ang@gmail.com>
2025-02-21 19:11:24 -0800

    Initial commit

---
+++ JourneyLog/JourneyLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  JourneyLogApp.swift
+//  JourneyLog
+//
+//  Created by Oscar Angulo on 2/21/25.
+//
+
+import SwiftUI
+
+@main
+struct JourneyLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JourneyLogTests/JourneyLogTests.swift
@@ -0,0 +1,17 @@
+//
+//  JourneyLogTests.swift
+//  JourneyLogTests
+//
+//  Created by Oscar Angulo on 2/21/25.
+//
+
+import Testing
+@testable import JourneyLog
+
+struct JourneyLogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
